8c6b0a157849128bfdc570f73b8b29248981fdf6
br-monteiro <bruno.monteirodg@gmail.com>
2016-07-25 22:55:44 -0300

    Item de orçamento

---
+++ 01-chain-of-responsability/Item.php
@@ -0,0 +1,23 @@
+<?php
+
+class Item
+{
+    private $valor;
+    private $nome;
+
+    public function __construct($nome, $valor)
+    {
+        $this->nome = $nome;
+        $this->valor = $valor;
+    }
+
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    public function getValor()
+    {
+        return $this->valor;
+    }
+}
